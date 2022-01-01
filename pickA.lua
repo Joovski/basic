c231a94c63301849399afedd4f61c04671fb17d1
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-01 16:05:46 +0100

    Create pickA.lua

---
+++ pickA.lua
@@ -0,0 +1,11 @@
+_G.toggle = true  --- change it to false to stop the script
+while _G.toggle == true do  
+wait()
+for i, v in pairs (game.Workspace.tools:GetChildren()) do
+if v:IsA("Tool") and v:FindFirstChild("Handle")    
+then
+v:FindFirstChild("Handle").CFrame =  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+   wait(0.3)
+end
+end
+end
