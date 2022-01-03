81810b665a8c479cc6990576ed27b51d1b00a187
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:45:00 +0100

    Update pickA.lua

--- pickA.lua
+++ pickA.lua
@@ -9,3 +9,19 @@ v:FindFirstChild("Handle").CFrame =  game.Players.LocalPlayer.Character.Humanoid
 end
 end
 end
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
