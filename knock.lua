67d60726324ab06e486f634b9a70a852ad11a60d
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:44:35 +0100

    Update knock.lua

--- knock.lua
+++ knock.lua
@@ -4,3 +4,19 @@ if v.Name == 'SetPlayerRagdolled' then
 v:Destroy()
 end
 end
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
