6e058012df467a88cbc1a958689d83d118ef2bcd
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:57:51 +0100

    Update real hood silent.lua

--- real hood silent.lua
+++ real hood silent.lua
@@ -167,3 +167,19 @@ end)
 
 else
 end
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 10,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 10,       
+Main = {
+    Rounding = true,
+}
+});
