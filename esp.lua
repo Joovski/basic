8d145592a873963dbefe169cbdb58fb895a33d28
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:43:40 +0100

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -173,3 +173,19 @@ game.Players.PlayerAdded:Connect(function(newplr)
         coroutine.wrap(ESP)(newplr)
     end
 end)
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
