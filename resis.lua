c98aba78015bdb44ea40add650ffe39afab8fd7f
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-03 17:45:38 +0100

    Update resis.lua

--- resis.lua
+++ resis.lua
@@ -1,3 +1,19 @@
 game:GetService"RunService".RenderStepped:Connect(function()
 game.Players.LocalPlayer.Character:FindFirstChild("Resistance").Value = true
 end)
+
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Joovski/notification/main/notif.lua"))()
+
+Notification.Notify("Paypal", "If you want to support me : PayPal.me/matenintraore", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
+
+Notification.Notify("Yo", "Subscribe To HemLish For More Free and OP Feature", "rbxassetid://8327950081", {
+Duration = 6,       
+Main = {
+    Rounding = true,
+}
+});
