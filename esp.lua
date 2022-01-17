766b04a034ef5d165ab66b912f781de905e871f3
Joovski <53435623+Joovski@users.noreply.github.com>
2022-01-17 18:22:18 +0100

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -173,19 +173,3 @@ game.Players.PlayerAdded:Connect(function(newplr)
         coroutine.wrap(ESP)(newplr)
     end
 end)
-
-wait(1.2)
-game.StarterGui:SetCore("SendNotification", {
-Title = "Information"; -- the title (ofc)
-Text = "Subscribe To HemLish For More Free and OP Feature"; -- what the text says (ofc)
-Icon = "rbxassetid://8327950081"; -- the image if u want. 
-Duration = 10; -- how long the notification should in secounds
-})
-
-wait(1.2)
-game.StarterGui:SetCore("SendNotification", {
-Title = "PayPal"; -- the title (ofc)
-Text = "If you want to support me : PayPal.me/matenintraore"; -- what the text says (ofc)
-Icon = "rbxassetid://8327950081"; -- the image if u want. 
-Duration = 10; -- how long the notification should in secounds
-})
